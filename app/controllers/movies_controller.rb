68c8e2f8a4bb3222047ba58076a2a700e428d9d2
Biru Xu <bx2198@columbia.edu>
2021-10-07 21:38:07 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all_ratings
     if params[:ratings]
       @ratings_to_show = params[:ratings].keys
     else 
